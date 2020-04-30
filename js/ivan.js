5a8869d5c9a08603f23b48a27e1c78e39b03de72
Darthsoviet <31465914+Darthsoviet@users.noreply.github.com>
2020-04-30 00:39:37 -0500

    v1.2.2

--- js/ivan.js
+++ js/ivan.js
@@ -1,26 +1,30 @@
 let url;
-window.onload=test;
-function test() {
+
+window.onload = () => {
     var list = document.getElementsByTagName("a");
-    for(let a of list) {
-        if(a.childElementCount!=0){
-            a.addEventListener("click",(e)=>{e.preventDefault()})
+    for (let a of list) {
+        if (a.childElementCount != 0) {
+            a.addEventListener("click", (e) => {
+                e.preventDefault()
+            })
         }
     }
 }
-function clicked(elemento){
-   url= elemento.getAttribute("href");
-    let modal=document.getElementById("modal");
-   let span= modal.getElementsByTagName("p")[0].getElementsByTagName("span")[0];
-    span.innerHTML=url;
-   
-    modal.style.display="block";
+
+function clicked(elemento) {
+    url = elemento.getAttribute("href");
+    let modal = document.getElementById("modal");
+    let span = modal.getElementsByTagName("p")[0].getElementsByTagName("span")[0];
+    span.innerHTML = url;
+
+    modal.style.display = "block";
 }
 
-function aceptar(){
+function aceptar() {
     window.location.replace(url);
 }
-function cancelar(){
-    let modal=document.getElementById("modal");
-    modal.style.display="none";
+
+function cancelar() {
+    let modal = document.getElementById("modal");
+    modal.style.display = "none";
 }
